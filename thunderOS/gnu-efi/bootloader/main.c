326f1cc26282a6cd2e67e94059cc6b5d3901f29c
thunderbear06 <68757348+thunderbear06@users.noreply.github.com>
2021-11-04 13:33:10 -0700

    Update main.c

--- thunderOS/gnu-efi/bootloader/main.c
+++ thunderOS/gnu-efi/bootloader/main.c
@@ -4,6 +4,55 @@
 
 typedef unsigned long long size_t;
 
+typedef struct {
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanLine;
+} Framebuffer;
+
+#define PSF1_MAGIC0 0x36
+#define PSF1_MAGIC1 0x04
+
+typedef struct 
+{
+	unsigned char magic[2];
+	unsigned char mode;
+	unsigned char charsize;
+} PSF1_HEADER;
+
+typedef struct 
+{
+	PSF1_HEADER* psf1_Header;
+	void* glyphBuffer;
+} PSF1_FONT;
+
+Framebuffer framebuffer;
+Framebuffer* InitializeGOP(){
+	EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
+	EFI_STATUS status;
+
+	status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void**)&gop);
+	if(EFI_ERROR(status)){
+		Print(L"!! GOP NOT FOUND!!\n\r");
+		return NULL;
+	}
+	else
+	{
+		Print(L"GOP Found\n\r");
+	}
+
+framebuffer.BaseAddress = (void*)gop->Mode->FrameBufferBase;
+framebuffer.BufferSize = gop->Mode->FrameBufferSize;
+framebuffer.Width = gop->Mode->Info->HorizontalResolution;
+framebuffer.Height = gop->Mode->Info->VerticalResolution;
+framebuffer.PixelsPerScanLine = gop->Mode->Info->PixelsPerScanLine;
+
+	return &framebuffer;
+}
+
 EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable){
 	EFI_FILE* LoadedFile;
 
@@ -25,6 +74,39 @@ EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EF
 	return LoadedFile;
 }
 
+PSF1_FONT* LoadPSF1Font(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
+	EFI_FILE* font = LoadFile(Directory, Path, ImageHandle, SystemTable);
+	if (font == NULL) return NULL;
+
+	PSF1_HEADER* fontHeader;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_HEADER), (void**)&fontHeader);
+	UINTN size = sizeof(PSF1_HEADER);
+	font->Read(font, &size, fontHeader);
+
+	if (fontHeader->magic[0] != PSF1_MAGIC0 || fontHeader->magic[1] != PSF1_MAGIC1){
+		return NULL;
+	}
+	
+	UINTN glyphBufferSize = fontHeader->charsize * 256;
+	if (fontHeader->mode == 1) {//512 glyph mode
+		glyphBufferSize = fontHeader->charsize * 512;
+	}
+
+	void* glyphBuffer;
+	{
+		font->SetPosition(font, sizeof(PSF1_HEADER));
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, glyphBufferSize, (void**)&glyphBuffer); 
+		font->Read(font, &glyphBufferSize, glyphBuffer);
+	}
+
+	PSF1_FONT* finishedFont;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_FONT), (void**)&finishedFont);
+	finishedFont->psf1_Header = fontHeader;
+	finishedFont->glyphBuffer = glyphBuffer;
+	return finishedFont;
+
+}
+
 int memcmp(const void* aptr, const void* bptr, size_t n){
 	const unsigned char* a = aptr, *b = bptr;
 	for (size_t i = 0; i < n; i++){
@@ -42,7 +124,7 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
 
 	if (Kernel == NULL) {
-		Print(L"!!FAILED TO LOAD KERNEL!! \n\r");
+		Print(L"FAILED TO LOAD KERNEL! Now what did you do? \n\r");
 	}
 	else {
 		Print(L"Kernel has been loaded successfully \n\r");
@@ -99,9 +181,29 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 
 	Print(L"Kernel has been loaded\n\r");
 
-	int (*KernelStart)() = ((__attribute__((sysv_abi)) int (*)() ) header.e_entry);
+	void (*KernelStart)(Framebuffer*, PSF1_FONT*) = ((__attribute__((sysv_abi)) void (*)(Framebuffer*, PSF1_FONT*) ) header.e_entry);
 
-	Print(L"%d\r\n", KernelStart());
+	PSF1_FONT* newFont = LoadPSF1Font(NULL, L"zap-light16.psf", ImageHandle, SystemTable);
+	if (newFont == NULL){
+		Print(L"Failed to load font, either font is not valid or can't be found!\n\r", newFont->psf1_Header->charsize);
+	}
+	else
+	{
+		Print(L"Font loaded successfully! char size = %d\n\r");
+	}
+
+	Framebuffer* newBuffer = InitializeGOP();
+
+	Print(L"Base: 0x%x\n\rSize: 0x%x\n\rWidth: %d\n\rHeight: %d\n\rPixelsPerScanLine: %d\n\r", 
+		newBuffer->BaseAddress, 
+		newBuffer->BufferSize, newBuffer->Width, 
+		newBuffer->Height, 
+		newBuffer->PixelsPerScanLine 
+	);
+
+	
+
+	KernelStart(newBuffer, newFont);
 
 	return EFI_SUCCESS ; // Exit the UEFI application
 }
